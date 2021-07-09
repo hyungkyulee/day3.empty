350f6fd44efbfa6eaf8014af1643f0ad13d715e5
Hyungkyu Lee <hyungkyu.lee@quartix.net>
2021-07-09 21:32:54 +0100

    initial project

---
+++ day3.conditional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day3.conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! sdfsdfsdfsdfdsfsdfsd");
+        }
+    }
+}
